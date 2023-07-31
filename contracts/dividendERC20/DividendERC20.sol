ce47b540efcdae475aa965895de049fbfb5d2c40
riemann <aleqvids@gmail.com>
2023-07-31 13:49:30 +0300

    updates

--- contracts/dividendERC20/DividendERC20.sol
+++ contracts/dividendERC20/DividendERC20.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract DividendToken is ERC20 {
-    // Mint 1 000 000 CFSH tokens
     uint constant _initial_supply = 1e18 * 1e6;
 
     string _name = "Sberbank Dividend Paying AMM token";
